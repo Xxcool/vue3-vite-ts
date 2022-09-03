0df8d31df17049588af9403e6cb08f9b9305525d
夏彬 <bin.xia@hcr.com.cn>
2022-09-03 14:27:23 +0800

    🌟feat(eslint): 代码优化

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,8 @@ declare module '@vue/runtime-core' {
     NButton: typeof import('naive-ui')['NButton']
     NCard: typeof import('naive-ui')['NCard']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
+    NDialogProvider: typeof import('naive-ui')['NDialogProvider']
+    NLoadingBarProvider: typeof import('naive-ui')['NLoadingBarProvider']
     NMessageProvider: typeof import('naive-ui')['NMessageProvider']
     NNotificationProvider: typeof import('naive-ui')['NNotificationProvider']
     NSpace: typeof import('naive-ui')['NSpace']
